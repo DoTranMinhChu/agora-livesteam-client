cba12726eb4e5c9590e2e75294d5ef5e1a1bc1dd
dotranminhchu <dotranminhchu@gmail.com>
2023-03-14 21:20:49 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,258 @@
+
+
+import React, { useEffect, useState } from "react";
+import {
+  AgoraVideoPlayer,
+  createClient,
+  createMicrophoneAndCameraTracks,
+  ClientConfig,
+  IAgoraRTCRemoteUser,
+  ICameraVideoTrack,
+  IMicrophoneAudioTrack,
+} from "agora-rtc-react";
+import axios from "axios";
+
+function makeid(length: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+const config: ClientConfig = {
+  mode: 'rtc', codec: "vp8",
+};
+
+const appId: string = "deb87355aa6647739307678644d833cb"; //ENTER APP ID HERE
+
+
+const App = () => {
+  const [inCall, setInCall] = useState(false);
+  const [channelName, setChannelName] = useState("");
+  const [host, setHost] = useState<boolean>(false)
+  return (
+    <div>
+      <h1 className="heading">Agora RTC NG SDK React Wrapper</h1>
+      {inCall ? (
+        <VideoCall setInCall={setInCall} channelName={channelName} host={host} />
+      ) : (
+        <ChannelForm setInCall={setInCall} setChannelName={setChannelName} setHost={setHost} />
+      )}
+    </div>
+  );
+};
+
+// the create methods in the wrapper return a hook
+// the create method should be called outside the parent component
+// this hook can be used the get the client/stream in any component
+const useClient = createClient(config);
+const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
+
+const VideoCall = (props: {
+  setInCall: React.Dispatch<React.SetStateAction<boolean>>;
+  host: boolean;
+  channelName: string;
+}) => {
+  const { setInCall, channelName } = props;
+  const [users, setUsers] = useState<IAgoraRTCRemoteUser[]>([]);
+  const [start, setStart] = useState<boolean>(false);
+  const { host } = props
+
+  // using the hook to get access to the client object
+  const client = useClient();
+
+  // ready is a state variable, which returns true when the local tracks are initialized, untill then tracks variable is null
+  const { ready, tracks } = useMicrophoneAndCameraTracks();
+
+  useEffect(() => {
+    console.log('role is', host)
+    // function to initialise the SDK
+    let init = async (name: string) => {
+      if (host === false) {
+
+        await client.setClientRole("audience");
+        client.on("user-published", async (user, mediaType) => {
+
+          await client.subscribe(user, mediaType);
+          console.log("subscribe success");
+          if (mediaType === "video") {
+            setUsers((prevUsers) => {
+              return [...prevUsers, user];
+
+            });
+
+          }
+          if (mediaType === "audio") {
+            user.audioTrack?.play();
+
+          }
+        });
+
+        client.on("user-unpublished", (user, type) => {
+          console.log("unpublished", user, type);
+          if (type === "audio") {
+            user.audioTrack?.stop();
+          }
+          if (type === "video") {
+            setUsers((prevUsers) => {
+              return prevUsers.filter((User) => User.uid !== user.uid);
+            });
+          }
+        });
+
+        client.on("user-left", (user) => {
+          console.log("leaving", user);
+          setUsers((prevUsers) => {
+            return prevUsers.filter((User) => User.uid !== user.uid);
+          });
+        });
+      }
+
+    };
+    const useruuid = makeid(5)
+    if (ready && tracks) {
+      console.log("init ready");
+      init(channelName);
+      const role = host ? 'publisher' : 'audience'
+      axios.get(`https://hitek-01.hitek.com.vn:8012/api/v1/agora/rtc/${channelName}/${role}/userAccount/${useruuid}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        }
+      }).then(res => {
+        let token = res.data.results.object.rtcToken;
+        console.log("Error to acquire", token)
+        client.join(appId, channelName, token, null);
+        if (tracks && host === true) {
+          client.setClientRole("host");
+          client.publish([tracks[0], tracks[1]]);
+        }
+        setStart(true);
+      })
+    }
+
+  }, [channelName, client, ready, tracks]);
+
+
+  return (
+    <div className="App">
+      {ready && tracks && (
+        <Controls tracks={tracks} setStart={setStart} setInCall={setInCall} />
+      )}
+      {start && tracks && <Videos users={users} tracks={tracks} host={host} />}
+    </div>
+  );
+};
+
+const Videos = (props: {
+  users: IAgoraRTCRemoteUser[];
+  tracks: [IMicrophoneAudioTrack, ICameraVideoTrack];
+  host: boolean;
+}) => {
+  const { users, tracks, host } = props;
+
+  return (
+
+    <div>
+      <div id="videos">
+        <AgoraVideoPlayer className='vid' videoTrack={tracks[1]} style={{ height: '65%', width: '65%' }} />
+        {users.length > 0 &&
+          users.map((user) => {
+            if (user.videoTrack) {
+              return (
+                <AgoraVideoPlayer className='vid' videoTrack={user.videoTrack} key={user.uid} style={{ height: '25%', width: '25%' }} />
+              );
+            } else return null;
+          })}
+      </div>
+    </div>
+  );
+};
+
+
+export const Controls = (props: {
+  tracks: [IMicrophoneAudioTrack, ICameraVideoTrack];
+  setStart: React.Dispatch<React.SetStateAction<boolean>>;
+  setInCall: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const client = useClient();
+  const { tracks, setStart, setInCall } = props;
+  const [trackState, setTrackState] = useState({ video: true, audio: true });
+
+  const mute = async (type: "audio" | "video") => {
+    if (type === "audio") {
+      await tracks[0].setEnabled(!trackState.audio);
+      setTrackState((ps) => {
+        return { ...ps, audio: !ps.audio };
+      });
+    } else if (type === "video") {
+      await tracks[1].setEnabled(!trackState.video);
+      setTrackState((ps) => {
+        return { ...ps, video: !ps.video };
+      });
+    }
+  };
+
+  const leaveChannel = async () => {
+    await client.leave();
+    client.removeAllListeners();
+    // we close the tracks to perform cleanup
+    tracks[0].close();
+    tracks[1].close();
+    setStart(false);
+    setInCall(false);
+  };
+
+  return (
+    <div className="controls">
+      <p className={trackState.audio ? "on" : ""}
+        onClick={() => mute("audio")}>
+        {trackState.audio ? "MuteAudio" : "UnmuteAudio"}
+      </p>
+      <p className={trackState.video ? "on" : ""}
+        onClick={() => mute("video")}>
+        {trackState.video ? "MuteVideo" : "UnmuteVideo"}
+      </p>
+      {<p onClick={() => leaveChannel()}>Leave</p>}
+    </div>
+  );
+};
+
+const ChannelForm = (props: {
+  setInCall: React.Dispatch<React.SetStateAction<boolean>>;
+  setChannelName: React.Dispatch<React.SetStateAction<string>>;
+  setHost: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const { setInCall, setChannelName, setHost } = props;
+
+  return (
+    <form className="join">
+      {appId === '' && <p style={{ color: 'red' }}>Please enter your Agora App ID in App.tsx and refresh the page</p>}
+      <input type="text"
+        placeholder="Enter Channel Name"
+        onChange={(e) => setChannelName(e.target.value)}
+      />
+      <button onClick={(e) => {
+        e.preventDefault();
+        setInCall(true);
+        setHost(true);
+      }}>
+        Join as Host
+      </button>
+      <button onClick={(e) => {
+        e.preventDefault();
+        setInCall(true);
+        setHost(false);
+      }}>
+        Join as Audience
+      </button>
+    </form>
+  );
+};
+
+export default App;
